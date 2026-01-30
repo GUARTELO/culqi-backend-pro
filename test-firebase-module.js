3f06c1223d2e6951c0c8033999e0842995d58e5c
Ciro Briones <cirobriones99@gmail.com>
2026-01-30 10:47:02 -0500

    Clean repository - Remove sensitive files and test files

--- test-firebase-module.js
+++
@@ -1,9 +0,0 @@
-﻿const firebase = require("./src/core/config/firebase.js");
-console.log("Firebase status:", firebase.getStatus());
-console.log("Firestore disponible:", !!firebase.firestore);
-
-if (firebase.firestore) {
-  firebase.firestore.listCollections()
-    .then(cols => console.log("Colecciones:", cols.length))
-    .catch(err => console.log("Error:", err.message));
-}
